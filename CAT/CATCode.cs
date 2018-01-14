fe30c9707c8273bb660a13732dda1706a752bf39
JuliaStroud93 <julia.stroud1993@gmail.com>
2018-01-14 23:27:15 +0000

    Adding CATObjects into CAT.

---
+++ CAT/CATCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CATObjects;
+
+namespace CAT
+{
+    public class CATCode
+    {
+        private CATModel m_Model = null;
+
+        public CATCode(string dataFilePath, string drumInitPath = "")
+        {
+            m_Model = new CATModel(IsotopeDataLoader.LoadDataFile(dataFilePath), IsotopeDataLoader.LoadDrumInitFile(drumInitPath));
+        }
+
+        public int GetRecommendedTimeStep()
+        {
+            return m_Model.Drum.RecommendedTimeStep;
+        }
+
+    }
+}
